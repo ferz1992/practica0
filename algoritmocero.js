1956561dcda76a7e827c6b01ac11f35f62337f9f
Your nombre <you@ejemplo.com>
2022-04-22 11:23:55 -0300

    algoritmoss

--- algoritmocero.js
+++ algoritmocero.js
@@ -4,16 +4,35 @@
 // extraer valores 
 //algoritmo para encender moto electrica.
 
-        <p>1-Tener una motocicleta para encender
-        2-Tener la llave que encienda esa motocicleta
+       /* //1-Tener una motocicleta para encender
+         2-Tener la llave que encienda esa motocicleta
         3-Dirigirme hacia la motocicleta 
         4-sentarme en el asiento de la motocicleta 
         5-ponerme en posicion de manejo
-        4-Buscar el switch donde va la llave de la motocicleta
-        5-Colocar la llave en el switch
-        6- girar hacia la derecha hasta llegar al primer tope
-        6-Buscar el boton de encendido electrico
-        7-Presionar el boton de encendido 
-        </p>
-        //
-var
+        6-Buscar el switch donde va la llave de la motocicleta
+        7-Colocar la llave en el switch
+        8- girar hacia la derecha hasta llegar al primer tope
+        9-Buscar el boton de encendido electrico
+        10-Presionar el boton de encendido 
+        // */
+        
+  //variables
+
+    var miMoto = true
+    console.log ( miMoto )
+
+    var tengoLlave = true
+
+    var EnciendeMoto = true
+  
+    console.log(tengoLlave
+        )
+        console.log ( EnciendeMoto)
+    
+
+    document.write(`
+    <h1> Motocicleta a encender: ${ miMoto } </h1>
+    <h1> ¿Tengo la llave?: ${ tengoLlave } </h1>
+    <h1> ¿Enciende la moto?: ${ EnciendeMoto } </h1>
+
+    `)
