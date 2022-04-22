e1f3fdc0a4e7264598ebdada2d196c757f838a44
Your nombre <you@ejemplo.com>
2022-04-22 09:44:58 -0300

    cambios en el algoritmo

---
+++ algoritmocero.js
@@ -0,0 +1,19 @@
+//creo variables de mi algoritmo/informacion
+//practica 1
+
+// extraer valores 
+//algoritmo para encender moto electrica.
+
+        <p>1-Tener una motocicleta para encender
+        2-Tener la llave que encienda esa motocicleta
+        3-Dirigirme hacia la motocicleta 
+        4-sentarme en el asiento de la motocicleta 
+        5-ponerme en posicion de manejo
+        4-Buscar el switch donde va la llave de la motocicleta
+        5-Colocar la llave en el switch
+        6- girar hacia la derecha hasta llegar al primer tope
+        6-Buscar el boton de encendido electrico
+        7-Presionar el boton de encendido 
+        </p>
+        //
+var
