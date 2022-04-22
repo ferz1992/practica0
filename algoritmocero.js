4355e46def58c9616fb136d0650f8ef774c587cf
Your nombre <you@ejemplo.com>
2022-04-22 11:59:18 -0300

    algoritmocero.js

--- algoritmocero.js
+++ algoritmocero.js
@@ -36,3 +36,46 @@
     <h1> ¿Enciende la moto?: ${ EnciendeMoto } </h1>
 
     `)
+
+    //problema 2
+
+    var manzMaria = 5
+    var manzPedro = 8
+    var manzSusana = 9
+    var totalManzanas = 5+8+9
+
+    console.log(totalManzanas )
+
+    document.write(`
+    <p> total de manzanas en la canasta: ${ totalManzanas } </p>
+    `)
+    //practica 3
+
+    var hamburguesaPrecio = 35
+    var refrescoPrecio = 18
+
+    var pagoTotal = 35+18
+    
+    console.log(pagoTotal)
+    document.write(`
+    <p> paco pago en total: ${ pagoTotal }
+    
+    `)
+    //problema 4
+    var cajaGalletas = 4
+    var galletasEnCaja = 5
+    var totalGalletas = 5*4
+    console.log(totalGalletas)
+    document.write(`
+    <p> total de galletas : ${totalGalletas}
+    `)
+    //problema5
+    var hastaToledoEnMilimetros = 2600000 
+    var hastaTiaEnMilimetros = 691000
+    var distRecoEnMilimetrosJuan = 2600000 + 691000
+
+    console.log (distRecoEnMilimetrosJuan)
+    
+    document.write(`
+    <p>la distancia que recorrio juan en milimetros : ${distRecoEnMilimetrosJuan}</p>
+    `)
